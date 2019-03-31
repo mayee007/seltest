4b19f7605fd873af8007c87f48c408247201829e
mayee <yelekeri@gmail.com>
2019-03-31 10:33:39 -0400

    first commit

---
+++ src/main/java/com/mine/seltest/SeltestApplication.java
@@ -0,0 +1,13 @@
+package com.mine.seltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeltestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeltestApplication.class, args);
+	}
+
+}
